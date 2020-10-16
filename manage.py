1781b43a872e9518ae179e30e9e640a059d87cf6
Leonardo Giordani <giordani.leonardo@gmail.com>
2020-10-16 19:31:25 +0100

    Step 2-4 - Initialise the testing database

--- manage.py
+++ manage.py
@@ -7,6 +7,8 @@ import subprocess
 import time
 
 import click
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 
 # Ensure an environment variable exists and has a value
@@ -16,10 +18,21 @@ def setenv(variable, default):
 
 setenv("APPLICATION_CONFIG", "development")
 
+APPLICATION_CONFIG_PATH = "config"
+DOCKER_PATH = "docker"
+
+
+def app_config_file(config):
+    return os.path.join(APPLICATION_CONFIG_PATH, f"{config}.json")
+
+
+def docker_compose_file(config):
+    return os.path.join(DOCKER_PATH, f"{config}.yml")
+
 
 def configure_app(config):
     # Read configuration from the relative JSON file
-    with open(os.path.join("config", f"{config}.json")) as f:
+    with open(app_config_file(config)) as f:
         config_data = json.load(f)
 
     # Convert the config into a usable Python dictionary
@@ -49,27 +62,33 @@ def flask(subcommand):
         p.wait()
 
 
-def docker_compose_cmdline(config):
-    configure_app(os.getenv("APPLICATION_CONFIG"))
+def docker_compose_cmdline(commands_string=None):
+    config = os.getenv("APPLICATION_CONFIG")
+    configure_app(config)
 
-    docker_compose_file = os.path.join("docker", f"{config}.yml")
+    compose_file = docker_compose_file(config)
 
-    if not os.path.isfile(docker_compose_file):
-        raise ValueError(f"The file {docker_compose_file} does not exist")
+    if not os.path.isfile(compose_file):
+        raise ValueError(f"The file {compose_file} does not exist")
 
-    return [
+    command_line = [
         "docker-compose",
         "-p",
         config,
         "-f",
-        docker_compose_file,
+        compose_file,
     ]
+
+    if commands_string:
+        command_line.extend(commands_string.split(" "))
+
+    return command_line
 
 
 @cli.command(context_settings={"ignore_unknown_options": True})
 @click.argument("subcommand", nargs=-1, type=click.Path())
 def compose(subcommand):
-    cmdline = docker_compose_cmdline(os.getenv("APPLICATION_CONFIG")) + list(subcommand)
+    cmdline = docker_compose_cmdline() + list(subcommand)
 
     try:
         p = subprocess.Popen(cmdline)
@@ -79,26 +98,62 @@ def compose(subcommand):
         p.wait()
 
 
+def run_sql(statements):
+    conn = psycopg2.connect(
+        dbname=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+        host=os.getenv("POSTGRES_HOSTNAME"),
+        port=os.getenv("POSTGRES_PORT"),
+    )
+
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor = conn.cursor()
+    for statement in statements:
+        cursor.execute(statement)
+
+    cursor.close()
+    conn.close()
+
+
+def wait_for_logs(cmdline, message):
+    logs = subprocess.check_output(cmdline)
+    while message not in logs.decode("utf-8"):
+        time.sleep(0.1)
+        logs = subprocess.check_output(cmdline)
+
+
+@cli.command()
+def create_initial_db():
+    configure_app(os.getenv("APPLICATION_CONFIG"))
+
+    try:
+        run_sql([f"CREATE DATABASE {os.getenv('APPLICATION_DB')}"])
+    except psycopg2.errors.DuplicateDatabase:
+        print(
+            f"The database {os.getenv('APPLICATION_DB')} already exists and will not be recreated"
+        )
+
+
 @cli.command()
 @click.argument("filenames", nargs=-1)
 def test(filenames):
     os.environ["APPLICATION_CONFIG"] = "testing"
     configure_app(os.getenv("APPLICATION_CONFIG"))
 
-    cmdline = docker_compose_cmdline(os.getenv("APPLICATION_CONFIG")) + ["up", "-d"]
+    cmdline = docker_compose_cmdline("up -d")
     subprocess.call(cmdline)
 
-    cmdline = docker_compose_cmdline(os.getenv("APPLICATION_CONFIG")) + ["logs", "db"]
-    logs = subprocess.check_output(cmdline)
-    while "ready to accept connections" not in logs.decode("utf-8"):
-        time.sleep(0.1)
-        logs = subprocess.check_output(cmdline)
+    cmdline = docker_compose_cmdline("logs db")
+    wait_for_logs(cmdline, "ready to accept connections")
+
+    run_sql([f"CREATE DATABASE {os.getenv('APPLICATION_DB')}"])
 
     cmdline = ["pytest", "-svv", "--cov=application", "--cov-report=term-missing"]
     cmdline.extend(filenames)
     subprocess.call(cmdline)
 
-    cmdline = docker_compose_cmdline(os.getenv("APPLICATION_CONFIG")) + ["down"]
+    cmdline = docker_compose_cmdline("down")
     subprocess.call(cmdline)
 
 
