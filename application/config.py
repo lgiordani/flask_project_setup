e1af82cb302d669b1559b788c92aafeab1b427d5
Leonardo Giordani <giordani.leonardo@gmail.com>
2020-12-21 22:28:52 +0000

    Step 2-1 - Adding a database container

--- application/config.py
+++ application/config.py
@@ -6,6 +6,17 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     """Base configuration"""
 
+    user = os.environ["POSTGRES_USER"]
+    password = os.environ["POSTGRES_PASSWORD"]
+    hostname = os.environ["POSTGRES_HOSTNAME"]
+    port = os.environ["POSTGRES_PORT"]
+    database = os.environ["APPLICATION_DB"]
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql+psycopg2://{user}:{password}@{hostname}:{port}/{database}"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class ProductionConfig(Config):
     """Production configuration"""
